dea52cdd8786d1e1144abac64e1e927ed75e0a26
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-12 13:12:46 +0530

    Update

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cors = require('cors');
+const path = require('path'); 
+const bodyParser = require('body-parser');
+const ConnectDB = require('./config/db'); 
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+ConnectDB();
+  
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.get('/', (req, res) => {
+    res.send(`Server running on port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
